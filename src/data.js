1154882496c31a68832780cbd6bd30bfb1bbbc80
akdavila2 <anakarina.daviladavila@gmail.com>
2021-07-16 17:37:50 -0500

    se visualizan los poster

--- src/data.js
+++ src/data.js
@@ -1,12 +1,11 @@
-// estas funciones son de ejemplo
+// Toda la data es un objeto
+// - studio::: propiedad de tipo string
+// - films: propiedad de tipo Array donde cada item es un objeto en si mismo
+// - films[0] objetos de tipo string exceptuando peaople, locations y vehicles"  que son arreglos.
+export const getData = async() => {
+    const response = await fetch('/data/ghibli/ghibli.json');
+    const ghibli = await response.json();
+    console.log("data obtenida", ghibli);
+    return ghibli;
 
-import a from './data/ghibli/ghibli.js'
-
-export const example = () => {
-    console.log(a);
-    return 'example';
-};
-
-export const anotherExample = () => {
-    return 'OMG';
-};
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-example();
-console.log(example, data);
+import { getData } from './data.js';
+//getData();
+const templatePoster = (data) => {
+    return `
+    <picture>
+    <img src= "${data.poster}" alt= "${data.title}" class= "imgfilm">
+    </picture>
+  `;
+}
+const start = async() => {
+    let data = await getData();
+    let output = [];
+    const gridMovies = document.querySelector('#gridMovies');
+    const div = document.createElement('div'); //createElement() crea un elemento HTML especificado por su tagName.
+    data.films.forEach((items) => output += templatePoster(items)); //forEach recorre los elementos del arreglo films.
+    div.innerHTML = output;
+    gridMovies.appendChild(div); //el metodo appendChild inserta un nuevo nodo dentro de la estructura DOM.
+};
+start();
