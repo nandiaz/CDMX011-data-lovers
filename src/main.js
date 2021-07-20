2f86733ab57a0895ac4cbfa168392c11a51d5a9e
akdavila2 <anakarina.daviladavila@gmail.com>
2021-07-20 10:00:02 -0500

    nuevas funciones a main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,42 @@
 import { getData } from './data.js';
-//getData();
+
 const templatePoster = (data) => {
-    return `
-    <picture>
+    return `<picture>
     <img src= "${data.poster}" alt= "${data.title}" class= "imgfilm">
-    </picture>
-  `;
+    </picture>`;
 }
 const start = async() => {
     let data = await getData();
-    let output = [];
+    let outputPoster = [];
     const gridMovies = document.querySelector('#gridMovies');
     const div = document.createElement('div'); //createElement() crea un elemento HTML especificado por su tagName.
-    data.films.forEach((items) => output += templatePoster(items)); //forEach recorre los elementos del arreglo films.
-    div.innerHTML = output;
+    data.films.forEach((items) => outputPoster += templatePoster(items)); //forEach recorre los elementos del arreglo films.
+    div.innerHTML = outputPoster;
     gridMovies.appendChild(div); //el metodo appendChild inserta un nuevo nodo dentro de la estructura DOM.
 
+    //extrayendo años y productores
+
+    const { films } = data;
+
+    let years = films.map((item) => item.release_date).filter((item) => ![undefined].includes(item));
+    years = [...new Set(years)];
+    console.log('los años ', years);
+    let producer = films.map((item) => item.producer).filter((item) => ![undefined].includes(item));
+    producer = [...new Set(producer)];
+    console.log('los productores ', producer);
+    let score = films.map((item) => item.score).filter((item) => ![undefined].includes(item));
+    producer = [...new Set(producer)];
+    console.log('los productores ', producer);
+
+    const createOpcion = (value, text) => {
+        const option = document.createElement('option');
+        option.value = value;
+        option.text = text;
+        return option
+    }
+    const select = document.querySelector('#orderDate');
+    const selectProducer = document.querySelector('#selectProducer');
+    years.forEach(item => select.add(createOpcion(item, item)));
+    producer.forEach(item => selectProducer.add(createOpcion(item, item)));
 };
 start();
