d363f9eb066a425e10801d8bfbe0ae1c9bc368d8
akdavila2 <anakarina.daviladavila@gmail.com>
2021-07-22 10:14:27 -0500

    interfaz de ususario lista css

--- src/data.js
+++ src/data.js
@@ -2,22 +2,65 @@
 // - studio::: propiedad de tipo string
 // - films: propiedad de tipo Array donde cada item es un objeto en si mismo
 // - films[0] objetos de tipo string exceptuando peaople, locations y vehicles"  que son arreglos.
-export const getData = async() => { //funcion es asincrona
-        const response = await fetch('/data/ghibli/ghibli.json'); //El método fetch() es una peticion get o post a una url en nuestro caso es local.
-        const ghibli = await response.json(); // respuesta de la funcion asincrona, por medio de la palabra await
-        console.log("data obtenida", ghibli);
-        return ghibli;
 
+//Objeto Data con un metodo Get
+//La ventaja de los objetos es que como tienen identidad, pueden encargarse de mas cosas dentro de el mismo
+
+export function DataManager() {
+    //Ready sera true cuando ya tienes la data disponible y data contiene tiene la "data"
+    this.data = undefined; //
+    this.ready = false;
+    const process = () => {
+            //destructuro el films para no llamarlo siempre data.films.forEach
+            const { films } = this.data;
+            this.films = films;
+            this.years = films.map((item) => item.release_date).filter((item) => ![undefined].includes(item));
+            this.years = [...new Set(this.years)]; //Un valor en un Set sólo puede estar una vez, el operador de descanso: ... lo que hace es recorrer los elementos de un objeto iterable y devolverlos separados por coma.
+            console.log('los años ', this.years);
+            this.producer = films.map((item) => item.producer).filter((item) => ![undefined].includes(item));
+            this.producer = [...new Set(this.producer)];
+            console.log('los productores', this.producer);
+        }
+        //Metodo para cargar la data.
+    this.load = async() => { //funcion es asincrona
+            const response = await fetch('/data/ghibli/ghibli.json'); //El método fetch() es una peticion get o post a una url en nuestro caso es local.
+            this.data = await response.json(); // respuesta de la funcion asincrona, por medio de la palabra await
+            console.log("data obtenida", this.data);
+            this.ready = true;
+            process();
+            return this.ready;
+        }
+        //Metodo filtrar por productor.
+    this.filterByProducer = (producer) => {
+            console.log('sirve por fa ', this.films);
+            if (!this.films) return []; //no se ha ejecutado el metodo load, no hay films para cargar.
+            return this.films.filter((item) => {
+                console.log("comparamos", `${item.producer} == ${producer}`)
+                return item.producer == producer
+            });
+        }
+        //Metodo ordenar ascendentes los films por release_date.
+    this.orderByYearsAscending = () => {
+            let yearsAscending = this.years.sort(function(a, b) {
+                return a - b;
+            })
+            return yearsAscending;
+        }
+        //Metodo ordenar descendente los films por release_date.
+    this.orderByYearsDescending = () => {
+        let yearsDescendant = this.years.reverse();
+        return yearsDescendant;
     }
-    // recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
-    // const filterData = (data, condition) => {
 
-//     }
-//     //El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+
+
+}
+
+
+
+//recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+//     //   
 // const sortData = (data, sortBy, sortOrder) => {
-
 //     }
 //     //permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada.
 // const computeStats = (data) => {
-
-// }
--- src/main.js
+++ src/main.js
@@ -1,42 +1,59 @@
-import { getData } from './data.js';
-
-const templatePoster = (data) => {
+import { DataManager, } from './data.js';
+let manager = new DataManager; /// se instancia todo lo que esta adentro del DataManager, todos los this, aqui podre verlos.
+console.log('la clase', manager);
+console.log('que trae producer', manager.filterByProducer('Hayao Miyazaki'))
+console.log('que trae años', manager.filterByProducer(1988))
+const templatePoster = (manager) => {
     return `<picture>
-    <img src= "${data.poster}" alt= "${data.title}" class= "imgfilm">
+    <img src= "${manager.poster}" alt= "${manager.title}" class= "imgfilm">
     </picture>`;
 }
+const createOpcion = (value, text) => {
+    const option = document.createElement('option');
+    option.value = value;
+    option.text = text;
+    return option;
+}
 const start = async() => {
-    let data = await getData();
+    await manager.load();
+    console.log('el manager', manager);
+    //Mostrando los poster
     let outputPoster = [];
     const gridMovies = document.querySelector('#gridMovies');
     const div = document.createElement('div'); //createElement() crea un elemento HTML especificado por su tagName.
-    data.films.forEach((items) => outputPoster += templatePoster(items)); //forEach recorre los elementos del arreglo films.
+    manager.films.forEach((items) => outputPoster += templatePoster(items)); //forEach recorre los elementos del arreglo films.
     div.innerHTML = outputPoster;
     gridMovies.appendChild(div); //el metodo appendChild inserta un nuevo nodo dentro de la estructura DOM.
-
-    //extrayendo años y productores
-
-    const { films } = data;
-
-    let years = films.map((item) => item.release_date).filter((item) => ![undefined].includes(item));
-    years = [...new Set(years)];
-    console.log('los años ', years);
-    let producer = films.map((item) => item.producer).filter((item) => ![undefined].includes(item));
-    producer = [...new Set(producer)];
-    console.log('los productores ', producer);
-    let score = films.map((item) => item.score).filter((item) => ![undefined].includes(item));
-    producer = [...new Set(producer)];
-    console.log('los productores ', producer);
-
-    const createOpcion = (value, text) => {
-        const option = document.createElement('option');
-        option.value = value;
-        option.text = text;
-        return option
-    }
+    //Llenando los select
     const select = document.querySelector('#orderDate');
     const selectProducer = document.querySelector('#selectProducer');
-    years.forEach(item => select.add(createOpcion(item, item)));
-    producer.forEach(item => selectProducer.add(createOpcion(item, item)));
-};
+    // manager.years.forEach(item => select.add(createOpcion(item, item)));
+    manager.producer.forEach(item => selectProducer.add(createOpcion(item, item)));
+    selectProducer.addEventListener('change', () => {
+        let optionProducer = selectProducer.value;
+        let filterProducer = manager.filterByProducer(optionProducer);
+        let listByProducer = '';
+        filterProducer.forEach((item) => listByProducer += templatePoster(item));
+        div.innerHTML = listByProducer;
+        gridMovies.appendChild(div)
+    })
+
+    select.addEventListener('change', () => {
+        let optionReleaseDate = select.value;
+        if (select.value == 'upward') {
+            let orderByReleaseData = manager.orderByYearsAscending();
+            console.log("años ascendentes", orderByReleaseData);
+            let listByReleaseData = '';
+            orderByReleaseData.forEach((item) => listByReleaseData += templatePoster(item));
+            div.innerHTML = listByReleaseData;
+            gridMovies.appendChild(div);
+        } else if (select.value == 'falling') {
+            let orderByReleaseData = manager.orderByYearsDescending();
+            let listByReleaseData = '';
+            orderByReleaseData.forEach((item) => listByReleaseData += templatePoster(item));
+            div.innerHTML = listByReleaseData;
+            gridMovies.appendChild(div);
+        }
+    })
+}
 start();
