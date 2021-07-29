7a086c2e3225e7be28ae253c2b103201c72db052
akdavila2 <anakarina.daviladavila@gmail.com>
2021-07-28 22:16:52 -0500

    nuevos archivos para implemntacion de segunda pagina1

--- src/template-poster.js
+++ src/template-poster.js
@@ -1,32 +1,20 @@
-import { moreInformation } from './moreinformation.js';
-console.log('llegando moreInformation a template', moreInformation);
-let manager = new moreInformation; /// se instancia todo lo que esta adentro del DataManager, todos los this, aqui podre verlos.
-console.log('la clase', manager);
-// `http://localhost:5000/slider?id=${manager.id}`;
-const URL = `${location.origin}/slider?id=${manager.id}`;
-export const templatePoster = manager => {
-        return `
+// `http://localhost:5000/slider?id=${poster.id}`;
+
+export const templatePoster = poster => {
+    const URL = `${location.origin}/slider?id=${poster.id}`;
+    return `
   <article class = "flip-card">
     <div class = "flip-card-inner">
       <section class = "flip-card front">
         <picture>
-        <img src= "${manager.poster}" alt= "${manager.title}" class= "imgfilm">
+        <img src= "${poster.poster}" alt= "${poster.title}" class= "imgfilm">
         </picture>
       </section>
       <section class = "flip-card-back">
-        <h3> ${manager.title} </h3>
-        <p> ${manager.description.slice(0, 150)}... <a  href="${URL}">Ver más</a> </p>
+        <h3> ${poster.title} </h3>
+        <p> ${poster.description.slice(0, 150)}... <a  href="${URL}">Ver más</a> </p>
       </section>
     </div>
   </article>
         `;
-    }
-    // //pagina2
-    // const moreInformation = getElementById('moreInformation');
-
-// const pageTwo = (posterId) => {
-//     select.addEventListener('click', event => {
-//         let seeMore = moreInformation.value;
-
-//     })
-// }
+}
