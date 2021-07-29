22c4ac20ea76227447b78c0621c83f8f26acdeca
nandiaz <nancy.090495@gmail.com>
2021-07-29 17:04:29 -0500

    boton regresar

--- src/main.js
+++ src/main.js
@@ -62,26 +62,7 @@ const start = async() => {
     btnFilter.addEventListener('click', toggleForm);
     btnTimes.addEventListener('click', toggleForm);
     
-    //pasar a la segunda pagina
-//     function filterFilmsSecond(id) {
-
-//         film= document.getElementById(id).value
-         
-         
-//          localStorage.setItem("Films", film);
-       
-//        }
-//        var film = localStorage.getItem("Films")
-       
-       
-//        var container2 = document.getElementById("container2")
-       
-//        var span = document.createElement("span")
-//        var contenido =document.createTextNode("Hola, " + film )
-//        container2.appendChild(span)
-//        span.appendChild(contenido)
-//        document.body.container2.appendChild(container2)
-// }
+   
 }
 
 start();
