9f61ab02928f9b990d76ad372d04d4ac97b351e0
nandiaz <nancy.090495@gmail.com>
2021-07-29 11:48:12 -0500

    probando 2a pagina
    new file:   src/main2.js
    	new file:   src/styleslider.css

--- src/main.js
+++ src/main.js
@@ -61,9 +61,29 @@ const start = async() => {
     btnTimes.addEventListener('click', toggleForm);
     
     //pasar a la segunda pagina
-    function filterFilmsSecond(id) {
+//     function filterFilmsSecond(id) {
 
+<<<<<<< HEAD
 }
+=======
+//         film= document.getElementById(id).value
+         
+         
+//          localStorage.setItem("Films", film);
+       
+//        }
+//        var film = localStorage.getItem("Films")
+       
+       
+//        var container2 = document.getElementById("container2")
+       
+//        var span = document.createElement("span")
+//        var contenido =document.createTextNode("Hola, " + film )
+//        container2.appendChild(span)
+//        span.appendChild(contenido)
+//        document.body.container2.appendChild(container2)
+// }
+>>>>>>> 60b08e5 (probando 2a pagina)
 
 start();
 
